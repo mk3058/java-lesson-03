2e311c3bb6c81568964f324a2b4fd5f505833348
mk3058 <kmk3058@icloud.com>
2024-03-28 13:17:09 +0900

    RollTheDice 솔루션 추가

--- src/main/java/kr/easw/lesson3/RollTheDice.java
+++ src/main/java/kr/easw/lesson3/RollTheDice.java
@@ -1,8 +1,10 @@
 package kr.easw.lesson3;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class RollTheDice {
+
     private static int[] frequency = new int[6];
 
     private static Random RANDOM = new Random();
@@ -18,24 +20,26 @@ public class RollTheDice {
     }
 
     /**
-     * 해당 메서드는 다음과 같은 역할을 가져야 합니다 :
-     * 주어진 값을 60으로 나눈 후, 나온 값의 수만큼 해당 인덱스에 존재하는 배열 값을 1 증가시켜야 합니다.
-     * 또한, 어떠한 경우에서도 주어진 기능을 구현했을 떄, 오류가 발생해서는 안됩니다.
+     * 해당 메서드는 다음과 같은 역할을 가져야 합니다 : 주어진 값을 60으로 나눈 후, 나온 값의 수만큼 해당 인덱스에 존재하는 배열 값을 1 증가시켜야 합니다. 또한,
+     * 어떠한 경우에서도 주어진 기능을 구현했을 떄, 오류가 발생해서는 안됩니다.
      * <p>
-     * 입력값은 일반적으로는 360을 넘지 않으나, 낮은 확률로 360을 넘습니다.
-     * 이러한 경우, extendArray 메서드를 구현하여 이를 이용해 배열을 재선언해야 합니다.
-     *
+     * 입력값은 일반적으로는 360을 넘지 않으나, 낮은 확률로 360을 넘습니다. 이러한 경우, extendArray 메서드를 구현하여 이를 이용해 배열을 재선언해야
+     * 합니다.
+     * <p>
      * 또한, 입력값이 double임으로 60으로 나눈 이후 int로 캐스팅이 필요합니다.
      */
     private static void fillArray(double result) {
-        throw new RuntimeException("이 코드 라인을 지우고, 이곳에서 작성하십시오.");
+        int res = (int) (result / 60);
+        if (res > 5) {
+            frequency = extendArray(res + 1);
+        }
+        frequency[res]++;
     }
 
     /**
-     * 해당 메서드는 다음과 같은 역할을 가져야 합니다 :
-     * 주어진 값의 크기만큼 배열을 생성한 후, 기존 배열에 있던 데이터를 복사해 반환해야 합니다.
+     * 해당 메서드는 다음과 같은 역할을 가져야 합니다 : 주어진 값의 크기만큼 배열을 생성한 후, 기존 배열에 있던 데이터를 복사해 반환해야 합니다.
      */
     private static int[] extendArray(int next) {
-        throw new RuntimeException("이 코드 라인을 지우고, 이곳에서 작성하십시오.");
+        return Arrays.copyOf(frequency, next);
     }
 }
